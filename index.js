b743f092d00ca5cc8b04c41e5e11c0e15312b98f
Anshul Garg <90211948+anshul6378@users.noreply.github.com>
2023-12-13 15:35:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
 
 var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
 
-var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
+var randomImageSource =randomDiceImage; //images/dice1.png - images/dice6.png
 
 var image1 = document.querySelectorAll("img")[0];
 
@@ -12,20 +12,20 @@ image1.setAttribute("src", randomImageSource);
 
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+var randomImageSource2 = "dice" + randomNumber2 + ".png";
 
 document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
 
 var randomNumber3 = Math.floor(Math.random() * 6) + 1;
 
-var randomImageSource3 = "images/dice" + randomNumber3 + ".png";
+var randomImageSource3 = "dice" + randomNumber3 + ".png";
 
 document.querySelectorAll("img")[2].setAttribute("src", randomImageSource3);
 
 var randomNumber4 = Math.floor(Math.random() * 6) + 1;
 
-var randomImageSource4 = "images/dice" + randomNumber4 + ".png";
+var randomImageSource4 = "dice" + randomNumber4 + ".png";
 
 document.querySelectorAll("img")[3].setAttribute("src", randomImageSource4);
 
